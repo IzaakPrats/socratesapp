38c3bea9baf56c6bf4f85d457b1d309ece8a6f2d
TerraTach <jeffrey.green.ii.2013@gmail.com>
2016-03-12 13:39:07 -0500

    Fully Commented
    Done

--- js/index.js
+++ js/index.js
@@ -111,30 +111,39 @@ function setSession(id, name, creator) {
 
     // Get Questions
     currentSessionRef.child("questions").on("child_added", function(snapshot) {
+      // Retrieve all the questions and their details
       var session = snapshot.val();
-
+      // Populate the lists with the questions
       var ul = document.getElementById("questionsList");
       var li = document.createElement("li");
+      // Set the id of the question list item equal to it's key
       li.setAttribute("id", snapshot.key());
 
+      // Create a link
       var a = document.createElement("a");
+      // Add the text and the script to run when the link is pressed.
       a.appendChild(document.createTextNode("Upvote this question"));
       a.setAttribute("href", "javascript:upvoteQuestion(\"" + snapshot.key() + "\")");
 
+      // Create a new paragraph element and populate it with the plain text of the question
       var p = document.createElement("p");
       p.appendChild(document.createTextNode(session.upvotes + " Upvotes : " + session.text));
 
+      // Populate the list item with the link to upvote and the question, then populate the ul with the list item
       li.appendChild(a);
       li.appendChild(p);
       ul.appendChild(li);
     });
 
+    // THIS SEEMS REPETITIVE ASK IZAAK ABOUT THIS
+    // Update the questions list when the child is changed
     currentSessionRef.child("questions").on("child_changed", function(snapshot) {
       var session = snapshot.val();
-
+      // Find the question element by it's UUID and erase it?
       var li = document.getElementById(snapshot.key());
       li.innerHTML = "";
 
+      // Create link element which runs script to upvote.
       var a = document.createElement("a");
       a.appendChild(document.createTextNode("Upvote this question"));
       a.setAttribute("href", "javascript:upvoteQuestion(\"" + snapshot.key() + "\")");
@@ -146,20 +155,25 @@ function setSession(id, name, creator) {
       li.appendChild(p);
     });
 
+    // Set the style to be that of when you're viewing a session.
     document.getElementById("backButton").style.display = 'inline';
     document.getElementById("sessionContainer").style.display = 'inline';
     document.getElementById("addQuestionButton").style.display = 'inline';
     document.getElementById("addSessionButton").style.display = 'none';
     document.getElementById("indexContainer").style.display = 'none';
   } else {
+    // If person was null, close the session.
     closeSession();
   }
 }
 
+// Script to increment upvote count
 function upvoteQuestion(id) {
+  // Retrieve question in 'questions' by 'id'
   var questionRef = currentSessionRef.child("questions").child(id);
   var qUpvotes;
 
+  //
   questionRef.on("value", function(snapshot) {
     var question = snapshot.val();
     qUpvotes = question.upvotes;
@@ -168,17 +182,22 @@ function upvoteQuestion(id) {
   questionRef.update({upvotes: qUpvotes + 1});
 }
 
+// Change style
 function openAddQuestion() {
   document.getElementById("addQuestionButton").style.display = 'none';
   document.getElementById("sessionContainer").style.display = 'none';
   document.getElementById("addQuestionContainer").style.display = 'inline';
 }
 
+// Function adds a question to a session given a form
 function addQuestion(form) {
   // If session is null, the user pressed cancel.
   if (form != null && form.text != null) {
+    // As long as the question isn't null
+    // Push up the new question to the session details on FireBase
     var newQuestion = currentSessionRef.child("questions").push();
 
+    // set the new questions details
     newQuestion.set({
       user_id: currentUserRef.key(),
       text: form.text.value,
@@ -186,6 +205,7 @@ function addQuestion(form) {
     });
   }
 
+  // Change style
   document.getElementById("addQuestionButton").style.display = 'inline';
   document.getElementById("sessionContainer").style.display = 'inline';
   document.getElementById("addQuestionContainer").style.display = 'none';
