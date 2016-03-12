e16d0083fd405a9b59e805ebf7c45a2b116ba4b8
Izaak Prats <izaak.prats@gmail.com>
2016-03-12 10:20:18 -0500

    The basics

--- js/index.js
+++ js/index.js
@@ -1,18 +1,38 @@
-var firebaseRef = new Firebase("https://burning-heat-1866.firebaseio.com/sessions/");
+window.onload = function() {
+  init();
+};
 
-firebaseRef.on("child_added", function(snapshot) {
-  var session = snapshot.val();
-  console.log("ID: " + session.id);
-  console.log("Name: " + session.name);
-  console.log("Creator: " + session.creator);
+function init() {
+  var firebaseRef = new Firebase("https://burning-heat-1866.firebaseio.com/sessions/");
+  var numOfSessions = 0;
 
-  var ul = document.getElementById("sessionsList");
-  var li = document.createElement("li");
-  
-  var a = document.createElement("a");
-  a.appendChild(document.createTextNode("Session : " + session.name));
-  a.setAttribute("href", "javascript:setSession("+ session.id +")");
+  firebaseRef.on("child_added", function(snapshot) {
+    var session = snapshot.val();
+    console.log("ID: " + session.id);
+    console.log("Name: " + session.name);
+    console.log("Creator: " + session.creator);
 
-  li.appendChild(a);
-  ul.appendChild(li);
-});
+    var ul = document.getElementById("sessionsList");
+    var li = document.createElement("li");
+
+    var a = document.createElement("a");
+    a.appendChild(document.createTextNode("Session : " + session.name));
+    a.setAttribute("href", "javascript:setSession("+ session.id +")");
+
+    li.appendChild(a);
+    ul.appendChild(li);
+
+    numOfSessions = numOfSessions + 1;
+    document.getElementById("sessionsCount").innerHTML = "There are " + numOfSessions + " Sessions."
+  });
+}
+
+function addASession(name, creator) {
+  var firebaseRef = new Firebase("https://burning-heat-1866.firebaseio.com/sessions/");
+  var newSessionRef = firebaseRef.push();
+
+  newSessionRef.set({
+    name: name,
+    creator: creator
+  });
+}
